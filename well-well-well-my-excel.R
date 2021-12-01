a15b721b092cc12a89339b430f97d6ab95b0ba3b
John MacKintosh <johnmackintosh@users.noreply.github.com>
2021-12-01 18:22:54 +0000

    Update well-well-well-my-excel.R
    add multiple sheet example

--- well-well-well-my-excel.R
+++ well-well-well-my-excel.R
@@ -5,31 +5,21 @@ suppressPackageStartupMessages(library(purrr))
 suppressPackageStartupMessages(library(data.table))
 
 
-files <- dir(pattern = "*.xlsx")  # in case anything else sneaks into folder
+files <- dir(pattern = "*.xlsx")  # in case other file types are in folder
 
-# base R for the lo-fi crew
+# base R f
 method_lapply <- lapply(files, read_excel)
 method_lapply <- do.call(rbind, Map(data.frame, method_lapply))
-head(method_lapply)
-
-# purrr for the tidyverts
-
-method_purrr <- map_dfr(files, read_excel, sheet = "Manchester")
-str(method_purrr)
 
 
-method_purrr2 <- map_dfr(files, read_excel, sheet = "London", .id = "source_wb")
-method_purrr2 %>% select(source_wb) %>% distinct()
-method_purrr2
+# purrr, using set_names to identify each source workbook
 
-# use set_names
-method_purrr3 <- files %>% 
-set_names() %>% 
-map_dfr(read_excel, sheet = "Manchester",.id = "source_wb")
-str(method_purrr3)
-method_purrr3 %>% select(source_wb) %>% distinct()
+method_purrr <- files %>% 
+  set_names() %>% 
+  map_dfr(read_excel, sheet = "Manchester",.id = "source_wb")
 
-# for loop and data.table for the hardcore
+
+# for loop and data.table 
 
 filecount <- as.numeric(length(files))  
 temp_list <- list()
@@ -40,19 +30,33 @@ for (i in seq_along(files)) {
   df$source_wb <- filename
   temp_list[[i]] <- df
   rm(df)
- method_datatable <- data.table::rbindlist(temp_list, fill = TRUE) 
+  method_datatable <- data.table::rbindlist(temp_list, fill = TRUE) 
 } 
-rm(temp_list)
-str(method_datatable)
-
+rm(temp_list);rm(filecount);rm(filename);rm(i)
 
 
 # rio 
 
-method_rio <-  import_list(files, rbind = TRUE, sheet = "Manchester")
-str(method_rio)
+method_rio <-  import_list(files, 
+                           rbind = TRUE, 
+                           rbind_label = "source_wb", #optional
+                           sheet = "Manchester") # will read the first sheet by default
 
-method_rio2 <-  import_list(files, rbind = TRUE, rbind_label = "source_wb", sheet = "London")
-str(method_rio2)
 
 
+
+
+#read in and combine all worksheets from one file
+
+path <- "copy2.xlsx"
+all_sheets <- path %>%
+  readxl::excel_sheets() %>%
+  purrr::set_names()
+
+# optional - exclude the Targets sheet which is different to the others
+all_sheets <- all_sheets[which(!all_sheets %like% 'Targets')]
+
+# combine the remaining sheets and identify them using .x
+all_sheets_df <- map_dfr(all_sheets,
+                    ~ read_excel(path, sheet = .x),
+                    .id = "sheet")
